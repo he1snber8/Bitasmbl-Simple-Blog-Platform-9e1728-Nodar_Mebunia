6142923e671e8b273c6e7c2f6ba19b13291cf09e
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-21 10:06:50 +0400

    Add App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostsPage from './PostsPage';
+function App(){
+  return (
+    <div>
+      <h1>Simple Blog Platform</h1>
+      <PostsPage />
+    </div>
+  );
+}
+export default App;
